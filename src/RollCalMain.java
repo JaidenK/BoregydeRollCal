87759544c85c639743bd9c19f6e92ec420876743
JaidenK <king.jaiden@yahoo.com>
2014-06-18 22:00:18 -0700

    Setup Main

---
+++ src/RollCalMain.java
@@ -0,0 +1,14 @@
+import king.jaiden.util.*;
+
+
+public class RollCalMain extends ApplicationWindow {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+}
